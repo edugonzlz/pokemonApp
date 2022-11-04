cbc9bc239bfd2edb06affbdb51480fbd710d6e7f
Eduardo González <eduardo.gonzalez@getwellat.com>
2022-11-04 20:40:41 +0100

    Removed PokemonService methods with result

--- Modules/PokemonServices/Sources/PokemonServices/Service/PokemonService.swift
+++ Modules/PokemonServices/Sources/PokemonServices/Service/PokemonService.swift
@@ -6,10 +6,6 @@ public protocol PokemonServiceProtocol {
     func save(pokemon: Pokemon)
     func remove(pokemonId: Int)
 
-    func getPokemons(pagination: PaginationParameters?, completion: @escaping (Result<PokemonList, Error>) -> Void)
-    func getPokemon(id: Int, completion: @escaping (Result<Pokemon, Error>) -> Void)
-    func getPokemon(name: String, completion: @escaping (Result<Pokemon, Error>) -> Void)
-
     func getPokemons(pagination: PaginationParameters?) -> AnyPublisher<PokemonList, Error>
     func getPokemon(id: Int) -> AnyPublisher<Pokemon, Error>
     func getPokemon(name: String) -> AnyPublisher<Pokemon, Error>
@@ -40,55 +36,6 @@ public extension PokemonService {
     }
 }
 
-//MARK - Result
-public extension PokemonService {
-    func getPokemons(pagination: PaginationParameters?, completion: @escaping (Result<PokemonList, Error>) -> Void) {
-        do {
-            let urlRequest = try Endpoints.getPokemons(pagination: pagination).makeRequest()
-            apiClient.request(urlRequest: urlRequest,
-                              completion: completion)
-        } catch {
-            completion(.failure(error))
-        }
-    }
-
-    func getPokemon(id: Int, completion: @escaping (Result<Pokemon, Error>) -> Void) {
-        if let pokemon = dataBase.pokemon(forId: id) {
-            return completion(.success(pokemon))
-        }
-
-        do {
-            let urlRequest = try Endpoints.getPokemonById(id).makeRequest()
-            apiClient.request(urlRequest: urlRequest,
-                              completion: completion)
-        } catch {
-            completion(.failure(error))
-        }
-    }
-
-    func getPokemon(name: String, completion: @escaping (Result<Pokemon, Error>) -> Void) {
-        if let pokemon = cache.value(forKey: name) {
-            return completion(.success(pokemon))
-        }
-
-        do {
-            let urlRequest = try Endpoints.getPokemonByName(name).makeRequest()
-            apiClient.request(urlRequest: urlRequest) { (result: (Result<Pokemon, Error>)) in
-                switch result {
-                case .success(let data):
-//                    self.cache.insert(data, forKey: data.name)
-                    completion(.success(data))
-                case .failure:
-                    completion(result)
-                }
-            }
-        } catch {
-            completion(.failure(error))
-        }
-    }
-}
-
-//MARK - Combine
 public extension PokemonService {
     func getPokemons(pagination: PaginationParameters?) -> AnyPublisher<PokemonList, Error> {
         do {
@@ -125,7 +72,7 @@ public extension PokemonService {
             let urlRequest = try Endpoints.getPokemonByName(name).makeRequest()
             return apiClient.request(urlRequest: urlRequest)
                 .map { (item: Pokemon) in
-//                    self.cache.insert(item, forKey: item.name)
+                    self.cache.insert(item, forKey: item.name)
                     return item
                 }.eraseToAnyPublisher()
 
